c4a5402e1ce7ecf5aa3afd3edb8a860c347e2c26
Anita Chan <anita82593@gmail.com>
2020-07-20 00:30:11 -0400

    Create JS file for Pixel Art Maker

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
-// Select color input
-// Select size input
+//Choose Grid Size
+const tableEl = document.getElementById('pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(gridHeight, gridWidth) {
+   for (r = 0; r < gridHeight; r++) {
+        let rowEl = document.createElement("tr");
+        for (c = 0; c < gridWidth; c++) {
+            let newCell = document.createElement("td");
+            rowEl.appendChild(newCell).className = "cell";
+        };
+        tableEl.appendChild(rowEl).className = "gridRow";
+    };
 }
+
+function submitForm(evt) {
+	evt.preventDefault();
+	const formEl = evt.srcElement;
+	const heightInputEl = formEl.querySelector('#inputHeight');
+	const height = parseInt(heightInputEl.value);
+	const widthInputEl = formEl.querySelector('#inputWidth');
+	const width = parseInt(widthInputEl.value);
+	makeGrid(height, width);
+}
+
+document.getElementById('sizePicker').onsubmit = submitForm;
+
+//Pick A Color
+const colorPickerEl = document.getElementById('colorPicker');
+let color = colorPickerEl.value;
+
+function changeColor(evt) {
+	color = colorPickerEl.value;
+}
+colorPickerEl.addEventListener('change', changeColor);
+
+//Design Canvas
+function fillInCell(evt) {
+	evt.target.style.backgroundColor = color;
+}
+tableEl.addEventListener('click', fillInCell);
